77162ba71c962a4f3f7a9dc27f1a96786a8775aa
MicroCountry <1570424984@qq.com>
2016-10-28 13:26:52 +0800

    Update CodeEnum.java

--- src/main/java/com/ddy/dianmai/ops/po/CodeEnum.java
+++ src/main/java/com/ddy/dianmai/ops/po/CodeEnum.java
@@ -20,7 +20,7 @@ package com.ddy.dianmai.ops.po;
 
 public enum CodeEnum {
 	SUCCESS(0,"SUCCESS"),LOGERROR(-1,"登录错误:%s%%"),SESSION_INVILAD(2,"会话失效，请重新登录。")
-	,LOGIN_FAIL(3,"登录失败。"),REQUEST_INVILAD(4,"请求无效");//add by wanggm 2016-04-25
+	,LOGIN_FAIL(3,"登录失败。"),REQUEST_INVILAD(4,"请求无效"),CODE_ERROR(5,"验证码错误"),USERPASS(6,"用户名或者密码错误");//add by wanggm 2016-04-25
 	private int value;
 	private String label;
 	private CodeEnum(int value,String label) {
